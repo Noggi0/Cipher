74d1a621af44213265314addad4c34cddbbf6fe0
vincent <vincent1.son@epitech.eu>
2021-06-15 11:13:04 +0200

    basic cipher

---
+++ main.cpp
@@ -0,0 +1,64 @@
+/*
+** SIDE PROJECT, 2021
+** cipher
+** File description:
+** main
+*/
+
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void print_help()
+{
+    std::cout << "Usage:\n\t./cipher [textToEncrypt] [-f 'pathToFileToEncrypt'] [-n 'sizeOfShift']" << std::endl;
+}
+
+std::string caesar_cipher(std::string *text, int shift)
+{
+    for (int i = 0; i < text->length(); i++)
+        text->at(i) += shift;
+    return *text;
+}
+
+std::string readFile(std::string filename)
+{
+    std::ifstream input(filename, std::ios_base::in);
+    std::string buffer;
+    std::string content;
+
+    while (std::getline(input, buffer))
+        content += buffer;
+    return content;
+}
+
+int main(int ac, char **av)
+{
+    int c = 0;
+    std::string text, filename, remaining;
+    bool fileMode = false;
+    int shift = 3;
+    while ((c = getopt(ac, av, "f:n:h")) != -1) {
+        switch (c) {
+            case 'f':
+                if (optarg) {
+                    filename = optarg;
+                    fileMode = true;
+                }
+            case 'n':
+                if (optarg)
+                    shift = atoi(optarg);
+            case 'h':
+                print_help();
+        }
+    }
+    if (optind != ac)
+        text = av[optind];
+    if (fileMode) {
+        text = readFile(filename);
+    }
+    caesar_cipher(&text, shift);
+    std::cout << text << std::endl;
+    return 0;
+}
